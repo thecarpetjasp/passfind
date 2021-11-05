cd231eba4d7ec165c069adc54bba6dc4744ac545
thecarpetjasp <71789855+thecarpetjasp@users.noreply.github.com>
2021-11-05 18:29:02 -0400

    Add files via upload

---
+++ install.py
@@ -0,0 +1,8 @@
+import os
+print ("Setting PassFind up for you...")
+os.system("cls||clear")
+os.system("sudo pip3 install colorama")
+os.system("sudo chmod +x passfind.py")
+os.system("sudo mv passfind.py /usr/bin/passfind")
+os.system("cls||clear")
+print ("Setup complete. Type \'passfind\' anywhere in your terminal to launch PassFind.")
